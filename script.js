5a677ff9dd4b1bac9e403024eccd0e5694fd7974
Mad Marine <pnnroman92@gmail.com>
2019-08-27 23:44:14 -0500

    app now generates a random number for players to guess towards

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+   var rand = Math.round(Math.random() + 8);
+  return rand;
+}
 
+const compareGuesses = (rand,user,computer)=> {
+  
+  rand = generateTarget();
+  var userDifference = rand - user;
+  var computerDifference = rand - computer;
+
+  if(userDifference > computerDifference){
+    return false;
+  } else if(userDifference === computerDifference){
+    return true;
+  } else {
+    return true;
+  }
+
+  
+
+
+}
